7c8bb6f124b8b9712eb25038c6307480cb983861
FinnTune <adieter@protonmail.com>
2023-08-21 14:03:17 +0300

    matrix

---
+++ matrix_transposition/src/lib.rs
@@ -0,0 +1,10 @@
+// This should be in the matrix_transposition library
+pub mod matrix_transposition {
+
+    #[derive(Debug, PartialEq, Eq)]
+    pub struct Matrix(pub (i32, i32), pub (i32, i32));
+
+    pub fn transpose(m: Matrix) -> Matrix {
+        Matrix((m.0 .0, m.1 .0), (m.0 .1, m.1 .1))
+    }
+}
