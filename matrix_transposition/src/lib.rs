3ec819007a65ade75228625c67f27802b895c0a7
FinnTune <adieter@protonmail.com>
2023-08-21 15:40:37 +0300

    added test program

--- matrix_transposition/src/lib.rs
+++ matrix_transposition/src/lib.rs
@@ -4,5 +4,5 @@
 pub struct Matrix(pub (i32, i32), pub (i32, i32));
 
 pub fn transpose(m: Matrix) -> Matrix {
-    Matrix((m.0 .0, m.1 .0), (m.0 .1, m.1 .1))
+    Matrix((m.0.0, m.1.0), (m.0.1, m.1.1))
 }
