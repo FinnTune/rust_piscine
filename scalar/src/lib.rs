2c9e3f97d90002f65e45b5581dd476794e7c9f2c
FinnTune <adieter@protonmail.com>
2023-08-21 10:59:17 +0300

    scalar

--- scalar/src/lib.rs
+++ scalar/src/lib.rs
@@ -10,18 +10,56 @@ pub fn pro(a: i8, b: i8) -> Result<i8, &'static str> {
     a.checked_mul(b).ok_or("ERROR: attempt to multiply with overflow")
 }
 
-pub fn quo(a: f32, b: f32) -> Result<f32, &'static str> {
-    if b == 0.0 {
-        Err("ERROR: attempt to divide by zero")
-    } else {
-        Ok(a / b)
-    }
+pub fn quo(a: f32, b: f32) -> f32 {
+    a / b
 }
 
-pub fn rem(a: f32, b: f32) -> Result<f32, &'static str> {
-    if b == 0.0 {
-        Err("ERROR: attempt to divide by zero")
-    } else {
-        Ok(a % b)
+pub fn rem(a: f32, b: f32) -> f32 {
+    a % b
+}
+
+mod scalar {
+    pub use super::*;
+}
+
+fn main() {
+    use scalar::*;
+
+    // sum
+    match sum(234, 2) {
+        Ok(result) => println!("sum : {}", result),
+        Err(error) => println!("sum : {}", error),
     }
+    match sum(1, 255) {
+        Ok(result) => println!("sum : {}", result),
+        Err(error) => println!("sum : {}", error),
+    }
+
+    // diff
+    match diff(234, 2) {
+        Ok(result) => println!("diff : {}", result),
+        Err(error) => println!("diff : {}", error),
+    }
+    match diff(-32768, 32766) {
+        Ok(result) => println!("diff : {}", result),
+        Err(error) => println!("diff : {}", error),
+    }
+
+    // pro
+    match pro(23, 2) {
+        Ok(result) => println!("pro : {}", result),
+        Err(error) => println!("pro : {}", error),
+    }
+    match pro(-128, 2) {
+        Ok(result) => println!("pro : {}", result),
+        Err(error) => println!("pro : {}", error),
+    }
+
+    // quo
+    println!("quo : {}", quo(22.0, 2.0));
+    println!("quo : {}", quo(-128.23, 2.0));
+
+    // rem
+    println!("rem : {}", rem(22.0, 2.0));
+    println!("rem : {}", rem(-128.23, 2.0));
 }
