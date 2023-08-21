ff8d685edb9e3c8c863c96c477b0b67a2664d048
FinnTune <adieter@protonmail.com>
2023-08-21 14:01:41 +0300

    fibonacci

---
+++ fibonacci2/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn fibonacci(n: u32) -> u32 {
+    if n == 0 {
+        return 0;
+    }
+    if n == 1 {
+        return 1;
+    }
+
+    let mut a = 0;
+    let mut b = 1;
+    for _ in 2..=n {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    b
+}
