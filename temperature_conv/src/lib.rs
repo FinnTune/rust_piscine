4cf160f35e949cd16208489cb71761301f83a417
FinnTune <adieter@protonmail.com>
2023-08-21 11:58:50 +0300

    temp

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,5 +1,6 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    (f - 32.0) * (5.0/9.0)
+    let celsius = (f - 32.0) * (5.0/9.0);
+    (celsius * 1e15).round() / 1e15  // Round to 15 decimal places
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
