64e30ea6c132a594a9825b952d36b6da971c172f
FinnTune <adieter@protonmail.com>
2023-08-22 16:09:41 +0300

    ownership

--- ownership/src/lib.rs
+++ ownership/src/lib.rs
@@ -1,11 +1,11 @@
-pub fn first_subword(mut s: String) -> String {
+pub fn first_subword(s: String) -> String {
     if s.contains('_') {
         return s.splitn(2, '_').next().unwrap().to_string();
     }
 
     for (i, ch) in s.char_indices() {
         if i != 0 && ch.is_uppercase() {
-            return s.splitn(2, ch).next().unwrap().to_string();
+            return s[..i].to_string();
         }
     }
 
