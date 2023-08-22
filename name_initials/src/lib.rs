8f4148edaabd1682cf17bd7b0ece915e16eae837
FinnTune <adieter@protonmail.com>
2023-08-22 16:04:32 +0300

    initials

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -1,11 +1,8 @@
-pub fn intials(names: Vec<&str>) -> Vec<String> {
-    let mut initials = Vec::new();
-    for name in names {
-        let mut initial = String::new();
-        for word in name.split_whitespace() {
-            initial.push_str(&word[..1]);
-        }
-        initials.push(initial);
-    }
-    initials
+pub fn initials(names: Vec<&str>) -> Vec<String> {
+    names.into_iter().map(|name| {
+        name.split_whitespace()
+            .map(|part| format!("{}.", part.chars().next().unwrap_or_default()))
+            .collect::<Vec<String>>()
+            .join(" ")
+    }).collect()
 }
