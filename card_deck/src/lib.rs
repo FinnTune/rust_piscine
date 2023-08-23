72ae898482823698776858ee9c6952f40ee0291a
FinnTune <adieter@protonmail.com>
2023-08-23 15:00:28 +0300

    card_deck

---
+++ card_deck/src/lib.rs
@@ -0,0 +1,76 @@
+extern crate rand;
+use rand::Rng;
+#[derive(Debug)]
+pub enum Suit {
+    Heart,
+    Diamond,
+    Spade,
+    Club,
+}
+#[derive(Debug)]
+pub enum Rank {
+    Ace,
+    Number(u8),
+    Jack,
+    Queen,
+    King,
+}
+
+impl Suit {
+    pub fn random() -> Suit {
+        match rand::thread_rng().gen_range(1, 4) {
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            4 => Suit::Club,
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn translate(value: u8) -> Suit {
+        match value {
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            4 => Suit::Club,
+            _ => panic!("Invalid value for Suit"),
+        }
+    }
+}
+
+impl Rank {
+    pub fn random() -> Rank {
+        match rand::thread_rng().gen_range(1, 13) {
+            1 => Rank::Ace,
+            2..=10 => Rank::Number(rand::thread_rng().gen_range(2, 10)),
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            13 => Rank::King,
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn translate(value: u8) -> Rank {
+        match value {
+            1 => Rank::Ace,
+            2..=10 => Rank::Number(value),
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            13 => Rank::King,
+            _ => panic!("Invalid value for Rank"),
+        }
+    }
+}
+#[derive(Debug)]
+pub struct Card {
+    pub suit: Suit,
+    pub rank: Rank,
+}
+
+pub fn winner_card(card: &Card) -> bool {
+    if let (&Suit::Spade, &Rank::Ace) = (&card.suit, &card.rank) {
+        true
+    } else {
+        false
+    }
+}
