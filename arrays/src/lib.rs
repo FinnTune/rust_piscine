d3d052af4beb269653c4ad2f731809043bc6f368
FinnTune <adieter@protonmail.com>
2023-08-23 15:09:35 +0300

    arrays

---
+++ arrays/src/lib.rs
@@ -0,0 +1,7 @@
+pub fn sum(a: &[i32]) -> i32 {
+    a.iter().sum()
+}
+
+pub fn thirtytwo_tens() -> [i32; 32] {
+    [10; 32]
+}
---
+++ test_arrays/src/main.rs
@@ -0,0 +1,16 @@
+use arrays::{sum, thirtytwo_tens};
+
+fn main() {
+    let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let a1: Vec<i32> = (1..11).collect();
+    let b = [5; 10];
+
+    println!("The Sum of the elements in {:?} = {}", a, sum(&a));
+    println!("The Sum of the elements in {:?} = {}", a1, sum(&a1));
+    println!("The Sum of the elements in {:?} = {}", b, sum(&b));
+    println!(
+        "Array size {} with only 10's in it {:?}",
+        thirtytwo_tens().len(),
+        thirtytwo_tens()
+    );
+}
