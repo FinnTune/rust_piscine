80a3154f13ba58fb6e4e41328a0cf2370eed9a7f
FinnTune <adieter@protonmail.com>
2023-08-29 11:28:55 +0300

    final task quest 4

--- boxing_todo/src/err.rs
+++ boxing_todo/src/err.rs
@@ -1,6 +1,5 @@
-use std::fmt;
-use std::fmt::Display;
 use std::error::Error;
+use std::fmt;
 
 #[derive(Debug)]
 pub enum ParseErr {
@@ -8,9 +7,18 @@ pub enum ParseErr {
     Malformed(Box<dyn Error>),
 }
 
-impl Display for ParseErr {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+impl fmt::Display for ParseErr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "Fail to parses todo")
+    }
+}
+
+impl Error for ParseErr {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            ParseErr::Empty => None,
+            ParseErr::Malformed(_) => Some(self),
+        }
     }
 }
 
@@ -19,23 +27,14 @@ pub struct ReadErr {
     pub child_err: Box<dyn Error>,
 }
 
-impl Display for ReadErr {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+impl fmt::Display for ReadErr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "Fail to read todo file")
-    }
-}
-
-impl Error for ParseErr {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        match self {
-            ParseErr::Empty => None,
-            ParseErr::Malformed(ref e) => Some(&**e),
-        }
     }
 }
 
 impl Error for ReadErr {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
-        Some(&*self.child_err)
+        Some(self.child_err.as_ref())
     }
 }
--- boxing_todo/src/lib.rs
+++ boxing_todo/src/lib.rs
@@ -1,19 +1,19 @@
-mod err;
-
-use err::{ParseErr, ReadErr};
-use serde::{Deserialize, Serialize};
-use std::error::Error;
+pub use std::error::Error;
 use std::fs::File;
 use std::io::Read;
+use std::path::Path;
+use json;
 
-#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+mod err;
+
+#[derive(Debug, Eq, PartialEq)]
 pub struct Task {
     pub id: u32,
     pub description: String,
     pub level: u32,
 }
 
-#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Eq, PartialEq)]
 pub struct TodoList {
     pub title: String,
     pub tasks: Vec<Task>,
@@ -21,20 +21,42 @@ pub struct TodoList {
 
 impl TodoList {
     pub fn get_todo(path: &str) -> Result<TodoList, Box<dyn Error>> {
-        let mut file = File::open(path).map_err(|e| ReadErr { child_err: Box::new(e) })?;
+        let path = Path::new(path);
+        let mut file = File::open(&path).map_err(|e| err::ReadErr {
+            child_err: Box::new(e),
+        })?;
         let mut contents = String::new();
-        file.read_to_string(&mut contents).map_err(|e| ReadErr { child_err: Box::new(e) })?;
-
-        let parsed: Result<TodoList, serde_json::Error> = serde_json::from_str(&contents);
-        match parsed {
-            Ok(todo_list) => {
-                if todo_list.tasks.is_empty() {
-                    Err(Box::new(ParseErr::Empty))
-                } else {
-                    Ok(todo_list)
-                }
+        file.read_to_string(&mut contents).map_err(|e| err::ReadErr {
+            child_err: Box::new(e),
+        })?;
+        let json = json::parse(&contents).map_err(|e| {
+            println!("{}", e);
+            err::ParseErr::Malformed(Box::new(e))
+        })?;
+        let title = json["title"].as_str().ok_or_else(|| {
+            err::ParseErr::Empty
+        })?.to_string();
+        let tasks = match json["tasks"].members().collect::<Vec<_>>() {
+            members if members.is_empty() => {
+                return Err(Box::new(err::ParseErr::Empty));
             }
-            Err(e) => Err(Box::new(ParseErr::Malformed(Box::new(e)))),
-        }
+            members => {
+                members
+                    .into_iter()
+                    .map(|task| {
+                        let id = task["id"].as_u32().unwrap();
+                        let description = task["description"].as_str().unwrap().to_string();
+                        let level = task["level"].as_u32().unwrap();
+                        Task {
+                            id,
+                            description,
+                            level,
+                        }
+                    })
+                    .collect()
+            }
+        };
+
+        Ok(TodoList { title, tasks })
     }
 }
