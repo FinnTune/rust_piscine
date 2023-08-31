46659ef27b1d49b14323416a2d6d5a0f5cf20e75
FinnTune <adieter@protonmail.com>
2023-08-31 16:35:58 +0300

    searching

---
+++ searching/src/lib.rs
@@ -0,0 +1,8 @@
+pub fn search(array: &[i32], key: i32) -> Option<usize> {
+        for (index, &element) in array.iter().enumerate() {
+            if element == key {
+                return Some(index);
+            }
+        }
+        None
+    }
