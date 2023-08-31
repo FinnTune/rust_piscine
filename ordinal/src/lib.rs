f7f4badb6c01e67e1ea7dc4ae2c2686ba17c4093
FinnTune <adieter@protonmail.com>
2023-08-31 16:54:59 +0300

    ordinal

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -1,5 +1,8 @@
 pub fn num_to_ordinal(x: u32) -> String {
 //Modulus operator x mod 10
+if x%100 == 11 || x%100 == 12 || x%100 == 13 {
+    format!("{}th", x)
+} else {
 if x%10 == 1 {
     format!("{}st", x)
 } else if x%10 == 2 {
@@ -8,5 +11,6 @@ if x%10 == 1 {
     format!("{}rd", x)
 } else {
     format!("{}th", x)
+}
 }
 }
